8e4a23e07ecca67989689f9c1055cc60f9c77ea4
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-18 16:01:33 +0500

    S1_hw_z1 done

---
+++ S1_hw_z1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое целое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(a);
+    Console.Write("Минимальное число: ");
+    Console.WriteLine(b);
+}
+if(a < b)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(b);
+    Console.Write("Минимальное число: ");
+    Console.WriteLine(a);
+}
