615c9f461002513392d2d48fab3855db191e7c29
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-18 13:30:49 +0500

    S1_з4

---
+++ Seminar1_задача4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите натуральное число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int neg_n = n*(-1);
+
+while(neg_n <= n)
+{
+    Console.WriteLine(neg_n);
+    neg_n++;
+}
