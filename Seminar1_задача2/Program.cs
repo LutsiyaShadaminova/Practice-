210c08b7a223b94f3d842038f3cf5387a09db95a
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-18 13:30:00 +0500

    S1_з2

---
+++ Seminar1_задача2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое целое число: ");
+
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat_value2 = value2 * value2;
+
+if(kvadrat_value2 == value1)
+{
+Console.WriteLine("Квадрат второго числа соответствует первому");
+}
+else
+{
+    Console.WriteLine("Искомых соответствий не выявлено");
+}
