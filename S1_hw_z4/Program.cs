a105fd5db4c70d9aa872fe1ebb430d7357dd5391
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-18 22:36:04 +0500

    S1_hw_z4 не разобралась

---
+++ S1_hw_z4/Program.cs
@@ -0,0 +1,12 @@
+﻿// с этой задачей не разобралась((
+
+Console.Write("Введите натуральное число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+while(n > a && (n % 2) == 0)
+{
+    Console.WriteLine(n);
+    n + 2;
+}
