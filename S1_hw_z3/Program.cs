4e7182d2a9bf6e809d7b4481056ee805933841fd
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-18 18:46:06 +0500

    S1_hw_z3 done

---
+++ S1_hw_z3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите натуральное целое число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n % 2 == 0)
+{
+    Console.Write("Является ли данное число четным: да");
+}
+else
+{
+    Console.Write("Является ли данное число четным: нет");
+}
