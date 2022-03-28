120268e073b1f27ab8bfd7a0730a36a381db7166
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-28 23:04:58 +0500

    done

---
+++ Seminar2_задача2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+int DelSecNum(int num)
+{
+    int n1 = (num / 100) * 10;
+    int n2 = num % 10;
+    return n1 + n2;  
+}
+
+    int number = new Random().Next(100, 999);
+    Console.WriteLine("Current number is " + number + " two-digit NO is "+ DelSecNum(number));
