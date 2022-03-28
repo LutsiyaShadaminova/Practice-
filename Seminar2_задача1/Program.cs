f3c194dde05b4c81d925531e4d2369c7c39ff6fc
Lutsiya Shadaminova <lutsiya.hairova@gmail.com>
2022-03-28 23:04:31 +0500

    done

---
+++ Seminar2_задача1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10; 99] и показывает наибольшую цифру числа.
+
+int FindMaxNum(int num)
+{
+    int n1 = num % 10;
+    int n2 = num / 10;
+
+    if(n1 > n2)
+    {
+        return n1;
+    }
+    else
+    {
+        return n2;
+    }
+}
+
+    int number = new Random().Next(10, 99);
+    Console.WriteLine("Current number is " + number + " max part is "+ FindMaxNum(number));
+
